beaee93a3dd276b58f7b943274bbadd759e80447
Zyven Zhao <zhaojuwen@deepin.com>
2019-06-20 17:21:24 +0800

    fix:modified response code to 200 for customized error

--- pkg/rest/controller/admin.go
+++ pkg/rest/controller/admin.go
@@ -22,14 +22,14 @@ func CreateMirror(c *gin.Context) {
 	err := c.BindJSON(&mirror)
 	if err != nil {
 		log.Errorf("Parse json mirror:%v found error:%v", mirror, err)
-		c.JSON(http.StatusBadRequest, utils.ErrorHelper(err, utils.PARAMETER_ERROR))
+		c.JSON(http.StatusOK, utils.ErrorHelper(err, utils.PARAMETER_ERROR))
 		return
 	}
 
 	err = mirror.CreateMirror()
 	if err != nil {
 		log.Errorf("Create mirror:%v found error:%v", mirror, err)
-		c.JSON(http.StatusBadRequest, utils.ErrorHelper(err, utils.CREATE_MIRROR_FAILED))
+		c.JSON(http.StatusOK, utils.ErrorHelper(err, utils.CREATE_MIRROR_FAILED))
 		return
 	}
 	c.JSON(http.StatusOK, utils.ResponseHelper(utils.SuccessResp()))
@@ -40,14 +40,14 @@ func DeleteMirror(c *gin.Context) {
 	id, err := strconv.Atoi(pathId)
 	if err != nil {
 		log.Errorf("Parse path param id:%d found error:%v", pathId, err)
-		c.JSON(http.StatusBadRequest, utils.ErrorHelper(err, utils.PARAMETER_ERROR))
+		c.JSON(http.StatusOK, utils.ErrorHelper(err, utils.PARAMETER_ERROR))
 		return
 	}
 
 	err = mirror2.DeleteMirror(id)
 	if err != nil {
 		log.Errorf("Delete mirror:%d found error:%v", id, err)
-		c.JSON(http.StatusBadRequest, utils.ErrorHelper(err, utils.DELETE_MIRROR_FAILED))
+		c.JSON(http.StatusOK, utils.ErrorHelper(err, utils.DELETE_MIRROR_FAILED))
 		return
 	}
 	c.JSON(http.StatusOK, utils.ResponseHelper(utils.SuccessResp()))
@@ -58,14 +58,14 @@ func UpdateMirror(c *gin.Context) {
 	err := c.BindJSON(&mirror)
 	if err != nil {
 		log.Errorf("Parse json mirror:%v found error:%v", mirror, err)
-		c.JSON(http.StatusBadRequest, utils.ErrorHelper(err, utils.PARAMETER_ERROR))
+		c.JSON(http.StatusOK, utils.ErrorHelper(err, utils.PARAMETER_ERROR))
 		return
 	}
 
 	err = mirror.UpdateMirror()
 	if err != nil {
 		log.Errorf("Update mirror:%v found error:%v", mirror, err)
-		c.JSON(http.StatusBadRequest, utils.ErrorHelper(err, utils.UPDATE_MIRROR_FAILED))
+		c.JSON(http.StatusOK, utils.ErrorHelper(err, utils.UPDATE_MIRROR_FAILED))
 		return
 	}
 	c.JSON(http.StatusOK, utils.ResponseHelper(utils.SuccessResp()))
@@ -77,19 +77,19 @@ func CreateTask(c *gin.Context) {
 	log.Infof("Creating task:%#v", task)
 	if err != nil {
 		log.Errorf("Parse json mirror:%v found error:%v", task, err)
-		c.JSON(http.StatusBadRequest, utils.ErrorHelper(err, utils.PARAMETER_ERROR))
+		c.JSON(http.StatusOK, utils.ErrorHelper(err, utils.PARAMETER_ERROR))
 		return
 	}
 	mirrors, err := mirror2.GetMirrorsByUpstream(task.Upstream)
 	if err != nil {
 		log.Errorf("Get mirrors by upstream in [%#v] found error:%v", mirrors, err)
-		c.JSON(http.StatusBadRequest, utils.ErrorHelper(err, utils.PARAMETER_ERROR))
+		c.JSON(http.StatusOK, utils.ErrorHelper(err, utils.PARAMETER_ERROR))
 		return
 	}
 	t, err := task.CreateTask()
 	if err != nil {
 		log.Errorf("Create task [%#v] found error:%v", task, err)
-		c.JSON(http.StatusBadRequest, utils.ErrorHelper(err, utils.CREATE_TASK_FAILED))
+		c.JSON(http.StatusOK, utils.ErrorHelper(err, utils.CREATE_TASK_FAILED))
 		return
 	}
 	var jobs configs.JobInfoList
@@ -108,7 +108,7 @@ func CreateTask(c *gin.Context) {
 		}.CreateCiTask()
 		if err != nil {
 			log.Errorf("Create ci task found error:%v", err)
-			c.JSON(http.StatusBadRequest, utils.ErrorHelper(err, utils.CREATE_CITASK_FAILED))
+			c.JSON(http.StatusOK, utils.ErrorHelper(err, utils.CREATE_CITASK_FAILED))
 			return
 		}
 	}
@@ -130,14 +130,14 @@ func GetTaskById(c *gin.Context) {
 	id, err := strconv.Atoi(pathId)
 	if err != nil {
 		log.Errorf("Parse path param id:%d found error:%v", pathId, err)
-		c.JSON(http.StatusBadRequest, utils.ErrorHelper(err, utils.PARAMETER_ERROR))
+		c.JSON(http.StatusOK, utils.ErrorHelper(err, utils.PARAMETER_ERROR))
 		return
 	}
 
 	task, err := task2.GetTaskById(id)
 	if err != nil {
 		log.Errorf("Get task by id:[%d] found error:%#v", id, err)
-		c.JSON(http.StatusBadRequest, utils.ErrorHelper(err, utils.FETCH_DATA_ERROR))
+		c.JSON(http.StatusOK, utils.ErrorHelper(err, utils.FETCH_DATA_ERROR))
 		return
 	}
 	var mirrorOperation operation.MirrorOperation
@@ -147,7 +147,7 @@ func GetTaskById(c *gin.Context) {
 	ciTasks, err := task2.GetCiTasksById(id)
 	if err != nil {
 		log.Errorf("Get ci tasks by id:[%d] found error:%#v", id, err)
-		c.JSON(http.StatusBadRequest, utils.ErrorHelper(err, utils.FETCH_DATA_ERROR))
+		c.JSON(http.StatusOK, utils.ErrorHelper(err, utils.FETCH_DATA_ERROR))
 		return
 	}
 	for _, ciTask := range ciTasks {
@@ -167,7 +167,7 @@ func GetIOpenTasks(c *gin.Context) {
 	tasks, err := task2.GetOpenTasks()
 	if err != nil {
 		log.Errorf("Get open tasks found error:%#v", err)
-		c.JSON(http.StatusBadRequest, utils.ErrorHelper(err, utils.FETCH_DATA_ERROR))
+		c.JSON(http.StatusOK, utils.ErrorHelper(err, utils.FETCH_DATA_ERROR))
 		return
 	}
 	for _, task := range tasks {
@@ -178,7 +178,7 @@ func GetIOpenTasks(c *gin.Context) {
 		ciTasks, err := task2.GetCiTasksById(task.Id)
 		if err != nil {
 			log.Errorf("Get ci tasks by id:[%d] found error:%#v", task.Id, err)
-			c.JSON(http.StatusBadRequest, utils.ErrorHelper(err, utils.FETCH_DATA_ERROR))
+			c.JSON(http.StatusOK, utils.ErrorHelper(err, utils.FETCH_DATA_ERROR))
 			return
 		}
 		for _, ciTask := range ciTasks {
@@ -196,7 +196,7 @@ func GetIOpenTasks(c *gin.Context) {
 func CheckAllMirrors(c *gin.Context) {
 	username, err := c.Cookie("username")
 	if err != nil {
-		c.JSON(http.StatusBadRequest, utils.ErrorHelper(err, utils.PARSE_COOKIE_ERROR))
+		c.JSON(http.StatusOK, utils.ErrorHelper(err, utils.PARSE_COOKIE_ERROR))
 		return
 	}
 	checker := checker.NewCDNChecker()
@@ -208,14 +208,14 @@ func CheckAllMirrors(c *gin.Context) {
 func CheckMirrorsByUpstream(c *gin.Context) {
 	username, err := c.Cookie("username")
 	if err != nil {
-		c.JSON(http.StatusBadRequest, utils.ErrorHelper(err, utils.PARSE_COOKIE_ERROR))
+		c.JSON(http.StatusOK, utils.ErrorHelper(err, utils.PARSE_COOKIE_ERROR))
 		return
 	}
 	upstream := c.Param("upstream")
 	mirrors, err := mirror2.GetMirrorsByUpstream(upstream)
 	if err != nil {
 		log.Errorf("Get mirrors by upstream:%s found error:%v", upstream, err)
-		c.JSON(http.StatusBadRequest, utils.ErrorHelper(err, utils.FETCH_DATA_ERROR))
+		c.JSON(http.StatusOK, utils.ErrorHelper(err, utils.FETCH_DATA_ERROR))
 		return
 	}
 	checker := checker.NewCDNChecker()
@@ -231,19 +231,19 @@ type MirrorsReq struct {
 func CheckMirrors(c *gin.Context) {
 	username, err := c.Cookie("username")
 	if err != nil {
-		c.JSON(http.StatusBadRequest, utils.ErrorHelper(err, utils.PARSE_COOKIE_ERROR))
+		c.JSON(http.StatusOK, utils.ErrorHelper(err, utils.PARSE_COOKIE_ERROR))
 		return
 	}
 	var mirrorsReq MirrorsReq
 	err = c.BindJSON(&mirrorsReq)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, utils.ErrorHelper(err, utils.PARAMETER_ERROR))
+		c.JSON(http.StatusOK, utils.ErrorHelper(err, utils.PARAMETER_ERROR))
 		return
 	}
 	mirrors, err := mirror2.GetMirrorsByIndices(mirrorsReq.Mirrors)
 	if err != nil {
 		log.Errorf("Get mirrors by indices:%#v found error:%v", mirrorsReq, err)
-		c.JSON(http.StatusBadRequest, utils.ErrorHelper(err, utils.FETCH_DATA_ERROR))
+		c.JSON(http.StatusOK, utils.ErrorHelper(err, utils.FETCH_DATA_ERROR))
 		return
 	}
 	checker := checker.NewCDNChecker()
@@ -257,12 +257,12 @@ func AbortTask(c *gin.Context) {
 	id, err := strconv.Atoi(pathId)
 	if err != nil {
 		log.Errorf("Parse path param id:%d found error:%v", pathId, err)
-		c.JSON(http.StatusBadRequest, utils.ErrorHelper(err, utils.PARAMETER_ERROR))
+		c.JSON(http.StatusOK, utils.ErrorHelper(err, utils.PARAMETER_ERROR))
 		return
 	}
 	err = task2.CloseTask(id)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, utils.ErrorHelper(err, utils.UPDATE_TASK_STATUS_FAILED))
+		c.JSON(http.StatusOK, utils.ErrorHelper(err, utils.UPDATE_TASK_STATUS_FAILED))
 		return
 	}
 	c.JSON(http.StatusOK, utils.ResponseHelper(utils.SuccessResp()))
@@ -273,26 +273,26 @@ func UpdateTaskStatus(c *gin.Context) {
 	id, err := strconv.Atoi(pathId)
 	if err != nil {
 		log.Errorf("Parse path param id:%d found error:%v", pathId, err)
-		c.JSON(http.StatusBadRequest, utils.ErrorHelper(err, utils.PARAMETER_ERROR))
+		c.JSON(http.StatusOK, utils.ErrorHelper(err, utils.PARAMETER_ERROR))
 		return
 	}
 	pathStatus := c.Param("status")
 	status, err := strconv.Atoi(pathStatus)
 	if err != nil {
 		log.Errorf("Parse path param id:%d found error:%v", pathId, err)
-		c.JSON(http.StatusBadRequest, utils.ErrorHelper(err, utils.PARAMETER_ERROR))
+		c.JSON(http.StatusOK, utils.ErrorHelper(err, utils.PARAMETER_ERROR))
 		return
 	}
 	err = task2.UpdateTaskStatus(id, constants.MirrorOperationStatus(status))
 	if err != nil {
 		log.Errorf("Update task status by task id:%d found error:%v", pathId, err)
-		c.JSON(http.StatusBadRequest, utils.ErrorHelper(err, utils.UPDATE_TASK_STATUS_FAILED))
+		c.JSON(http.StatusOK, utils.ErrorHelper(err, utils.UPDATE_TASK_STATUS_FAILED))
 		return
 	}
 	err = task2.CloseTask(id)
 	if err != nil {
 		log.Errorf("Update task status by task id:%d found error:%v", pathId, err)
-		c.JSON(http.StatusBadRequest, utils.ErrorHelper(err, utils.UPDATE_TASK_STATUS_FAILED))
+		c.JSON(http.StatusOK, utils.ErrorHelper(err, utils.UPDATE_TASK_STATUS_FAILED))
 		return
 	}
 	c.JSON(http.StatusOK, utils.ResponseHelper(utils.SuccessResp()))
@@ -303,13 +303,13 @@ func GetArchiveByTaskId(c *gin.Context) {
 	id, err := strconv.Atoi(pathId)
 	if err != nil {
 		log.Errorf("Parse path param id:%d found error:%v", pathId, err)
-		c.JSON(http.StatusBadRequest, utils.ErrorHelper(err, utils.PARAMETER_ERROR))
+		c.JSON(http.StatusOK, utils.ErrorHelper(err, utils.PARAMETER_ERROR))
 		return
 	}
 	archive, err := archive.GetArchiveByTaskId(id)
 	if err != nil {
 		log.Errorf("Get archive by id:[%d] found error:%#v", id, err)
-		c.JSON(http.StatusBadRequest, utils.ErrorHelper(err, utils.FETCH_DATA_ERROR))
+		c.JSON(http.StatusOK, utils.ErrorHelper(err, utils.FETCH_DATA_ERROR))
 		return
 	}
 	c.JSON(http.StatusOK, utils.ResponseHelper(utils.SetData("archive", archive)))
@@ -326,7 +326,7 @@ func GetAllArchives(c *gin.Context) {
 		archivePage, err1 := strconv.Atoi(page)
 		archiveLimit, err2 := strconv.Atoi(limit)
 		if err1 != nil || err2 != nil {
-			c.JSON(http.StatusBadRequest, utils.ErrorHelper(nil, utils.PARAMETER_ERROR))
+			c.JSON(http.StatusOK, utils.ErrorHelper(nil, utils.PARAMETER_ERROR))
 			return
 		}
 		GetPagedArchives(c, archivePage, archiveLimit)
@@ -335,7 +335,7 @@ func GetAllArchives(c *gin.Context) {
 	archives, err := archive.GetAllArchives()
 	if err != nil {
 		log.Errorf("Get archives found error:%#v", err)
-		c.JSON(http.StatusBadRequest, utils.ErrorHelper(err, utils.FETCH_DATA_ERROR))
+		c.JSON(http.StatusOK, utils.ErrorHelper(err, utils.FETCH_DATA_ERROR))
 		return
 	}
 	c.JSON(http.StatusOK, utils.ResponseHelper(utils.SetData("archives", archives)))
@@ -345,7 +345,7 @@ func GetArchivesCount(c *gin.Context) {
 	count, err := archive.GetArchivesCount()
 	if err != nil {
 		log.Errorf("Get archive page count found error:%#v", err)
-		c.JSON(http.StatusBadRequest, utils.ErrorHelper(err, utils.FETCH_DATA_ERROR))
+		c.JSON(http.StatusOK, utils.ErrorHelper(err, utils.FETCH_DATA_ERROR))
 		return
 	}
 	c.JSON(http.StatusOK, utils.ResponseHelper(utils.SetData("count", count)))
@@ -355,7 +355,7 @@ func GetPagedArchives(c *gin.Context, page, size int) {
 	archives, err := archive.GetPagedArchives(page, size)
 	if err != nil {
 		log.Errorf("Get archives found error:%#v", err)
-		c.JSON(http.StatusBadRequest, utils.ErrorHelper(err, utils.FETCH_DATA_ERROR))
+		c.JSON(http.StatusOK, utils.ErrorHelper(err, utils.FETCH_DATA_ERROR))
 		return
 	}
 	c.JSON(http.StatusOK, utils.ResponseHelper(utils.SetData("archives", archives)))
@@ -365,29 +365,29 @@ func Logout(c *gin.Context) {
 	username, err := c.Cookie("username")
 	if err != nil {
 		log.Errorf("Get cookie username found error:%#v", err)
-		c.JSON(http.StatusBadRequest, utils.ErrorHelper(err, utils.GET_COOKIE_ERROR))
+		c.JSON(http.StatusOK, utils.ErrorHelper(err, utils.GET_COOKIE_ERROR))
 		return
 	}
 	sessionId, err := c.Cookie("sessionId")
 	if err != nil {
 		log.Errorf("Get cookie sessionId found error:%#v", err)
-		c.JSON(http.StatusBadRequest, utils.ErrorHelper(err, utils.GET_COOKIE_ERROR))
+		c.JSON(http.StatusOK, utils.ErrorHelper(err, utils.GET_COOKIE_ERROR))
 		return
 	}
 	redisSessionId, err := redis.Get(username + "-session-id")
 	if err != nil {
 		log.Errorf("Get redis sessionId found error:%#v", err)
-		c.JSON(http.StatusBadRequest, utils.ErrorHelper(err, utils.FETCH_DATA_ERROR))
+		c.JSON(http.StatusOK, utils.ErrorHelper(err, utils.FETCH_DATA_ERROR))
 		return
 	}
 	if sessionId != redisSessionId {
-		c.JSON(http.StatusBadRequest, utils.ErrorHelper(err, utils.USER_NOT_LOGIN))
+		c.JSON(http.StatusOK, utils.ErrorHelper(err, utils.USER_NOT_LOGIN))
 		return
 	}
 	err = redis.Del(username + "-session-id")
 	if err != nil {
 		log.Errorf("Del redis sessionId found error:%#v", err)
-		c.JSON(http.StatusBadRequest, utils.ErrorHelper(err, utils.FETCH_DATA_ERROR))
+		c.JSON(http.StatusOK, utils.ErrorHelper(err, utils.FETCH_DATA_ERROR))
 		return
 	}
 	c.JSON(http.StatusOK, utils.ResponseHelper(utils.SuccessResp()))
@@ -397,23 +397,23 @@ func GetLoginStatus(c *gin.Context) {
 	username, err := c.Cookie("username")
 	if err != nil {
 		log.Errorf("Get cookie username found error:%#v", err)
-		c.JSON(http.StatusBadRequest, utils.ErrorHelper(err, utils.GET_COOKIE_ERROR))
+		c.JSON(http.StatusOK, utils.ErrorHelper(err, utils.GET_COOKIE_ERROR))
 		return
 	}
 	sessionId, err := c.Cookie("sessionId")
 	if err != nil {
 		log.Errorf("Get cookie sessionId found error:%#v", err)
-		c.JSON(http.StatusBadRequest, utils.ErrorHelper(err, utils.GET_COOKIE_ERROR))
+		c.JSON(http.StatusOK, utils.ErrorHelper(err, utils.GET_COOKIE_ERROR))
 		return
 	}
 	redisSessionId, err := redis.Get(username + "-session-id")
 	if err != nil {
 		log.Errorf("Get redis sessionId found error:%#v", err)
-		c.JSON(http.StatusBadRequest, utils.ErrorHelper(err, utils.FETCH_DATA_ERROR))
+		c.JSON(http.StatusOK, utils.ErrorHelper(err, utils.FETCH_DATA_ERROR))
 		return
 	}
 	if sessionId != redisSessionId {
-		c.JSON(http.StatusBadRequest, utils.ErrorHelper(err, utils.USER_NOT_LOGIN))
+		c.JSON(http.StatusOK, utils.ErrorHelper(err, utils.USER_NOT_LOGIN))
 		return
 	}
 	c.JSON(http.StatusOK, utils.ResponseHelper(utils.SetData("username", username)))
@@ -429,12 +429,12 @@ func SendMail(c *gin.Context) {
 	var mailReq MailReq
 	err := c.BindJSON(&mailReq)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, utils.ErrorHelper(err, utils.PARAMETER_ERROR))
+		c.JSON(http.StatusOK, utils.ErrorHelper(err, utils.PARAMETER_ERROR))
 		return
 	}
 	err = utils.SendMail(mailReq.Subject, mailReq.Content, mailReq.Mails...)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, utils.ErrorHelper(err, utils.UNKNOWN_ERROR))
+		c.JSON(http.StatusOK, utils.ErrorHelper(err, utils.UNKNOWN_ERROR))
 		return
 	}
 	c.JSON(http.StatusOK, utils.ResponseHelper(utils.SuccessResp()))
